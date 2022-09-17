26ac050c7d09db832c4468805bcfa8bbdac8432f
Fady Magdy <fadymagdy.developer@gmail.com>
2022-09-18 01:00:29 +0200

    fixed intro circle

--- src/components/openingshow/Openingshow.jsx
+++ src/components/openingshow/Openingshow.jsx
@@ -3,14 +3,7 @@ import "./openingshow.scss";
 export default function Openingshow() {
   return (
     <div className="openingshow">
-      <div className="doors">
-        <div className="left"></div>
-        <div className="right"></div>
-        <div className="top-bottom">
-          <div className="top"></div>
-          <div className="bottom"></div>
-        </div>
-      </div>
+      <div className="big-circle"></div>
       <div className="circle"></div>
     </div>
   );
